9c230ab6a7cd46b9e1899eca596944201444b425
choijungwoo493 <jwoo1016@gmail.com>
2022-06-06 21:48:06 +0900

    Initial commit

---
+++ src/main/java/hello/core/member/MemberApp.java
@@ -0,0 +1,15 @@
+package hello.core.member;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L,"memberA",Gradle.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(member.getId());
+        System.out.println(member);
+        System.out.println(findMember);
+
+
+    }
+}
---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    @Test
+    void join() {
+        Member member = new Member(1L,"memberA",Gradle.VIP);
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        Assertions.assertThat(member).isEqualTo(findMember); 
+    }
+
+    @Test
+    void findMember() {
+    }
+}
